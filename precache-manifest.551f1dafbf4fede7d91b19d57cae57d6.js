1eb325b45e3b81fd84052afcf727276d0435e0ac
RaulEK <kattai.raul@gmail.com>
2019-11-05 13:28:31 +0200

    Updates

---
+++ precache-manifest.551f1dafbf4fede7d91b19d57cae57d6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2d046164c94b0faa573f832d81e4d10",
+    "url": "/os-hw2/index.html"
+  },
+  {
+    "revision": "9c343e710509a0e9300e",
+    "url": "/os-hw2/static/css/main.1bfabc82.chunk.css"
+  },
+  {
+    "revision": "c16c4a36adfd7c97df82",
+    "url": "/os-hw2/static/js/2.95507347.chunk.js"
+  },
+  {
+    "revision": "9c343e710509a0e9300e",
+    "url": "/os-hw2/static/js/main.b8310ec3.chunk.js"
+  },
+  {
+    "revision": "8cddb6acb4a4a6d5d530",
+    "url": "/os-hw2/static/js/runtime-main.a442fe8e.js"
+  }
+]);
